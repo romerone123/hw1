e12471c41a9c527ba1fdd47cd5e6ebc447bc5545
Anthony Romero <romerone@usc.edu>
2023-01-25 22:38:35 +0000

    done with first part

--- split.cpp
+++ split.cpp
@@ -11,13 +11,31 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
-
 /* Add a prototype for a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+if(in == nullptr)
+  {
+    odds = nullptr;
+    evens = nullptr;
+    return;
+  }
+  
+  if(in->value % 2 != 0)
+  {
+    odds = in;
+		in = in->next;
+    split(in, odds->next, evens);
+  }
+  else if(in->value % 2 == 0)
+  {
+    evens = in;
+		in = in->next;
+    split(in, odds, evens->next);
+  }
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,39 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+
+void print(Node*& in)
+{
+  while(in != NULL)
+  {
+    std::cout << in->value << std::endl;
+    in = in->next;
+  }
+}
 
 int main(int argc, char* argv[])
 {
+	Node* in = nullptr;  
+ /* Node in2(7, nullptr);
+  Node in3(6, &in2);
+  Node in4(5, &in3);
+	Node in5(4, &in4);
+	Node in6(3, &in5);
+  Node in7(2, &in6);
+  Node in8(1, &in7);*/
+	Node in8(1, nullptr);
+  in = &in8; 
 
+  Node* odds = nullptr;
+  Node* evens = nullptr;
+
+  split(in, odds, evens);
+  std::cout << "Odd linked list:" << std::endl;
+  print(odds); 
+  std::cout << "Even linked list:" << std::endl;
+  print(evens); 
+  std::cout << "in: " << std::endl;
+	print(in);
+  return 0;
 }
